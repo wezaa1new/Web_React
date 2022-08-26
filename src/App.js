57f77539157398be36d3cbfc70c3f56f2e728faf
New <chanasorn.new@hotmail.com>
2022-08-26 10:49:54 +0700

    add func-component.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import {Header,Content,Footer} from './func-components'
+
+function App() {
+  return (
+    [<Header/>,<Content/>,<Footer/>]
+  );
+}
+
+export default App;
