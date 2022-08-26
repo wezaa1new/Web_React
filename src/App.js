fbb6eec7a0b6250080ed2c882ceb0e1bc9cee577
New <chanasorn.new@hotmail.com>
2022-08-26 11:25:34 +0700

    add class-props.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import {Header,Content,Footer} from './func-components'
+import MsgBox from './class-props';
 
 function App() {
   return (
-    [<Header/>,<Content/>,<Footer/>]
+    <MsgBox
+      text = "React & React Native"
+      color = "red"
+      bgColor = "#ccc"
+      fontSize = "18pt"
+      border = "solid 10px black"
+    />
   );
 }
 
---
+++ src/class-props.js
@@ -0,0 +1,17 @@
+import React,{Component} from "react";
+
+export default class MsgBox extends Component{
+    
+    render(){
+        const divStyle = {
+            margin : '10px',
+            paddind : '5px',
+            color : this.props.color,
+            backgroundColor : this.props.bgColor,
+            fontSize : this.props.fontSize,
+            border : this.props.border
+        }
+
+        return <div style={divStyle}>{this.props.text}</div>
+    }
+}
