a072a09ad21e6a900fa3f9816b8fb9c76e9c8f86
New <chanasorn.new@hotmail.com>
2022-09-09 11:39:48 +0700

    func

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import './App.css';
 import React from 'react'
 import {Header,Content,Footer} from './func-components'
 import MsgBox from './class-props';
+import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 function App() {
   return (
-    <MsgBox
-      text = "React & React Native"
-      color = "red"
-      bgColor = "#ccc"
-      fontSize = "18pt"
-      border = "solid 10px black"
-    />
+    <MessageBox/>
   );
 }
 
