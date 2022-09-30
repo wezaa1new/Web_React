8de636b4d387900039455bb9d1cb99c7c5504fcc
Chanasorn <chanasorn.new@hotmail.com>
2022-09-30 11:46:08 +0700

    form-bootstrap

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import {Header,Content,Footer} from './func-components'
 import MsgBox from './class-props';
 import RefsArray from './refs-array';
 import MessageBox from './state-func';
+import FormBootstrap from './form-bootstrap'
 
 function App() {
   return (
-    <MessageBox/>
+    <FormBootstrap/>
   );
 }
 
